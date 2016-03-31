e2a9f6e7735072af7ec21a30aeb2795acdbeaf1f
oscar <oscaroceguera@gmail.com>
2016-03-31 12:50:37 -0700

    refactor with beforeEach

--- ReduxSimpleStarter-master/test/components/app_test.js
+++ ReduxSimpleStarter-master/test/components/app_test.js
@@ -2,7 +2,7 @@ import { renderComponent, expect } from '../test_helper'
 import App from '../../src/components/app'
 
 // Use 'describe' to group together similar tests
-describe('Test the App Component', () => {
+describe('App Component', () => {
 
   // Use 'it' to test a single attribute of target
   it('show the correct text', () => {
--- ReduxSimpleStarter-master/test/components/comment_box_test.js
+++ ReduxSimpleStarter-master/test/components/comment_box_test.js
@@ -1,19 +1,24 @@
 import { renderComponent, expect } from '../test_helper'
 import CommentBox from '../../src/components/comments_box'
 
-describe('CommentsBox', () => {
+describe('Comments Box', () => {
+
+  let component; //undefined
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox)
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox)
     expect(component).to.have.class('commnet-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox)
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox)
     expect(component.find('button')).to.exist
   });
+
 });
